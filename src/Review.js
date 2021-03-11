590b96db15e800c51192bea400c2bf0c820d7b84
Joseph M <jmedina1985@hotmail.com>
2021-03-10 21:18:01 -0800

    Working on review component

--- src/Review.js
+++ src/Review.js
@@ -6,7 +6,13 @@ const Review = () => {
   //default to 0 because arrays are index based
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
-  return <h2>review component</h2>;
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} />
+      </div>
+    </article>
+  );
 };
 
 export default Review;
