1dabdfb4aa9c09589039bea1e4646fcadeee2bbb
Joseph M <jmedina1985@hotmail.com>
2021-03-10 21:14:58 -0800

    Working on review component

--- src/Review.js
+++ src/Review.js
@@ -3,6 +3,8 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
+  //default to 0 because arrays are index based
+  const [index, setIndex] = useState(0);
   return <h2>review component</h2>;
 };
 
