f11966e1b6ead829dc357c9e594c2e0b91e8a93a
Joseph M <jmedina1985@hotmail.com>
2021-03-10 21:19:30 -0800

    Working on review component

--- src/Review.js
+++ src/Review.js
@@ -9,7 +9,7 @@ const Review = () => {
   return (
     <article className="review">
       <div className="img-container">
-        <img src={image} alt={name} />
+        <img src={image} alt={name} className="person-img" />
       </div>
     </article>
   );
